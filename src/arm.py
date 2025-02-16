418511b1ee865387989173febf9813ce93df40a0
bschulte1515 <112735843+bschulte1515@users.noreply.github.com>
2025-02-16 15:01:29 -0600

    Create arm.py
    Just some commands to show how the arm moves

---
+++ src/arm.py
@@ -0,0 +1,48 @@
+import time
+import almath
+import argparse
+from naoqi import ALProxy
+
+class MyClass(GeneratedClass):
+  def init(self):
+    GeneratedClass.init(self)
+
+  def onLoad(self):
+    #put initialization code here
+    pass
+
+  def onUnload(self):
+    #put clean-up code here
+    pass
+
+  def onInput_onStart(self):
+    motion = ALProxy("ALMotion")
+    names = "RElbowYaw"
+    names2 = "RElbowRoll"
+    names3 = "RShoulderPitch"
+    names4 = "RShoulderRoll"
+    angles1 = 60.0*almath.TO_RAD
+    angles2 = 30.0*almath.TO_RAD
+    fractionMaxSpeed = 0.1
+    motion.setAngles(names,angles1,fractionMaxSpeed)
+    time.sleep(2.0)
+    motion.setAngles(names,angles2,fractionMaxSpeed)
+    time.sleep(2.0)
+    motion.setAngles(names2,angles1,fractionMaxSpeed)
+    time.sleep(2.0)
+    motion.setAngles(names2,angles2,fractionMaxSpeed)
+    time.sleep(2.0)
+    motion.setAngles(names3,angles1,fractionMaxSpeed)
+    time.sleep(2.0)
+    motion.setAngles(names3,angles2,fractionMaxSpeed)
+    time.sleep(2.0)
+    motion.setAngles(names4,angles1,fractionMaxSpeed)
+    time.sleep(2.0)
+    motion.setAngles(names4,angles2,fractionMaxSpeed)
+    time.sleep(2.0)
+    #print(motion.getBodyNames("Body"))
+    self.onStopped() #activate the output of the box
+
+  def onInput_onStop(self):
+    self.onUnload() #it is recommended to reuse the clean-up as the box is stopped
+    self.onStopped() #activate the output of the box
