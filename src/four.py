8a1f474e625e2082e62d005bfa4375549818580a
jDeBok <113641695+jDeBok@users.noreply.github.com>
2025-03-11 11:54:14 -0500

    Comment skeleton

--- src/four.py
+++ src/four.py
@@ -1,3 +1,15 @@
+#Prologue Comments
+#Code Artifact: LHand.py
+#Code Purpose:
+# Team 19
+#Created: 
+#Revised:
+#Preconditions:
+#Postconditions: 
+#Errors:
+#Side Effects:
+#Invariants:
+#Faults:
 class MyClass(GeneratedClass):
     def __init__(self):
         GeneratedClass.__init__(self)
