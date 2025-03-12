5cd0d9238bec25d49ea0e089d992ee3a64638555
jDeBok <113641695+jDeBok@users.noreply.github.com>
2025-03-12 13:13:33 -0500

    Added comments

--- src/move.py
+++ src/move.py
@@ -1,23 +1,23 @@
 #Prologue Comments
 #Code Artifact: move.py
-#Code Purpose:
+#Code Purpose: choose which column to place the next piece
 # Team 19
 #Created: 
-#Revised:
-#Preconditions:
-#Postconditions: 
-#Errors:
-#Side Effects:
-#Invariants:
-#Faults:
+#Revised: 3/12/25
+#Preconditions: Ro-board is set up to have taken a picture from the board
+#Postconditions: Ro-board knows what column to place next piece in
+#Errors: If the Ro-board is not connected to the internet, socket timeouts
+#Side Effects: None
+#Invariants: None
+#Faults: If there is a timeout may cause a bad return value
 from inference_sdk import InferenceConfiguration, InferenceHTTPClient
 import cv2
 import supervision as sv
 import requests
 import statistics as stats
 
-RED = '1'
-YELLOW = '2'
+RED = '1' #classifications for play markers
+YELLOW = '2' #other color for play markers
 
 # These are currently some hard-coded values but eventually they would
 # be pulled from the image taken from the robot and the chosen difficulty
@@ -29,25 +29,25 @@ def determine_move(board, difficulty):
     #TODO: Check for win and return magic value if game end
     # url_won = "https://kevinalbs.com/connect4/back-end/index.php/getMoves?board_data=" + board + "&player=" + YELLOW
 
-    url_move = "https://kevinalbs.com/connect4/back-end/index.php/getMoves?board_data=" + board + "&player=" + YELLOW
-    response_move = requests.get(url_move).json()
+    url_move = "https://kevinalbs.com/connect4/back-end/index.php/getMoves?board_data=" + board + "&player=" + YELLOW #URL for API
+    response_move = requests.get(url_move).json() #send request and receive response
 
-    vals = [int(response_move[i]) for i in '0123456']
+    vals = [int(response_move[i]) for i in '0123456'] #parse response
 
-    if difficulty == 'easy':
-        t = min(vals) 
-    elif difficulty == 'med':
-        t = stats.median(vals)
-    else: # difficulty == 'hard':
-        t = max(vals)
+    if difficulty == 'easy': #if gameplay is on easy, get the smallest column response
+        t = min(vals) #min value is worst
+    elif difficulty == 'med': #get medium response, which is medium correct column to choose.
+        t = stats.median(vals) #median value is median between worst and best
+    else: # difficulty == 'hard': #get the greatest column response
+        t = max(vals) #max value is best
 
-    for i in '0123456':
-        if int(response_move[i]) == t:
-            return i
+    for i in '0123456': #check each colomn number
+        if int(response_move[i]) == t: #check if equal to my column from difficulty choice
+            return i #return which column
 
-    return -1
+    return -1 #error, did not find t
 
-image = cv2.imread(FILE)
+image = cv2.imread(FILE) #read image from file
 
 # Define confidence threshold
 config = InferenceConfiguration(confidence_threshold=0.2)
@@ -56,33 +56,33 @@ config = InferenceConfiguration(confidence_threshold=0.2)
 CLIENT = InferenceHTTPClient(
     api_url="https://detect.roboflow.com",
     api_key="API_KEY"
-)
+) #define requirements for connecting with API, website and key
 
-CLIENT.configure(config)
-result = CLIENT.infer(image, model_id="connect4-ampe5/2")
+CLIENT.configure(config) #configure connection
+result = CLIENT.infer(image, model_id="connect4-ampe5/2") #get result from API
 
-detections = sv.Detections.from_inference(result)
-detections = detections[detections.class_id != 0]
+detections = sv.Detections.from_inference(result) #get inference from the API
+detections = detections[detections.class_id != 0] #filter out bad results
 
-pieces = [p for p in result['predictions'] if p['class'] in ['red','yellow','empty']]
-sorted_pieces = sorted(pieces, key=lambda d: d['y'], reverse=False)
-grouped_data = [sorted_pieces[i:i + 7] for i in range(0, len(sorted_pieces), 7)]
+pieces = [p for p in result['predictions'] if p['class'] in ['red','yellow','empty']] #sort pieces from result and current picture
+sorted_pieces = sorted(pieces, key=lambda d: d['y'], reverse=False) #sort pieces by color
+grouped_data = [sorted_pieces[i:i + 7] for i in range(0, len(sorted_pieces), 7)] #group them then by class.
 
-fully_sorted = []
-for group in grouped_data:
-    sorted_group = sorted(group, key=lambda d: d['x'], reverse=False)
-    fully_sorted.append(sorted_group)
+fully_sorted = [] #init sorted array
+for group in grouped_data: #now put groups in sorted array
+    sorted_group = sorted(group, key=lambda d: d['x'], reverse=False) #sort individual group
+    fully_sorted.append(sorted_group) #add to array
 
-board = ""
-for row in fully_sorted:
-    for column in row:
-        piece = column['class']
-        if piece == 'red':
-            board += RED
-        elif piece == 'yellow':
-            board += YELLOW
+board = "" #init board
+for row in fully_sorted: #now go through sorted array and change into enums
+    for column in row: #go through each column and change into enums
+        piece = column['class'] #get current piece
+        if piece == 'red': #if is in red class
+            board += RED #add enum RED to board
+        elif piece == 'yellow': #if in yellow class
+            board += YELLOW #add enum YELLOW to board
         else: # piece == 'empty':
-            board += '0'
+            board += '0' #add enum empty to board
 
-move = determine_move(board, DIFFICULTY)
-print(move)
+move = determine_move(board, DIFFICULTY) #call determine move function with my correct board and given difficulty
+print(move) #output which move the Ro-board should make
