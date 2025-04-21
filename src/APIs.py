975be0790a5476084671a78a98ccf37a0f4ead89
bschulte1515 <bschulte1515@gmail.com>
2025-04-20 22:03:59 -0500

    Update to APIs and board. Now we should be getting more accurate boards

--- src/APIs.py
+++ src/APIs.py
@@ -15,38 +15,33 @@ import supervision as sv #Used for RoboFlow
 import requests #used for connections
 import http.server #sets up HTTP server
 from PIL import Image #creates image file to send to Connect4 API
-from board import Board
-import numpy as np
+from board import Board # Used for board data structure
 
 RED = '1' #classifications for play markers
 YELLOW = '2' #other color for play markers
 
 def determine_move(board, difficulty) -> (int, int):
     # Check for a win
-    board = board[len(board)-42:] # Trim board down to 42 pieces to correct hallucinations
-    while len(board) < 42:
-        board = '0' + board
-    print(len(board))
-    b = Board(board)
-    game_state = b.win_exists()
+    b = Board(board) # Create new board object
+    board.print() # Print board for debugging
+    game_state = b.win_exists() # Use board to check for win
 
     if game_state != 0: # Game done
-        return game_state, -1
-    print(board)
-    print(len(board))
+        return game_state, -1 # Return game end and no column to play (-1)
+
     url_move = "https://kevinalbs.com/connect4/back-end/index.php/getMoves?board_data=" + board + "&player=" + RED #URL for API
     response_move = requests.get(url_move).json() #send request and receive response
-    print(response_move)
-    vals = [0]*7
-    curr_col = 0
-    while curr_col < 7:
-        if str(curr_col) in response_move:
-            vals[curr_col] = response_move[str(curr_col)]
+    print(response_move) # Print response for debugging
+    vals = [0]*7 # Create empty list for vals
+    curr_col = 0 # Start at 0th column
+    while curr_col < 7: # Loop through columns
+        if str(curr_col) in response_move: # Check for column in response
+            vals[curr_col] = response_move[str(curr_col)] # Use value if it is given
         else:
-            vals[curr_col] = -9999
-        curr_col += 1
+            vals[curr_col] = -9999 # Use dummy value if not given
+        curr_col += 1 # Go to next column
 
-    print(vals)
+    print(vals) # Print vals of columns for debugging
     if difficulty == "1": #if gameplay is on easy, get the smallest column response
         print("easy")
         t = min(vals) #min value is worst
@@ -60,10 +55,10 @@ def determine_move(board, difficulty) -> (int, int):
 
     for i in range(len(vals)): #check each colomn number
         if int(vals[i]) == t: #check if equal to my column from difficulty choice
-            print(t)
-            b.make_move(i)
-            game_state = b.win_exists()
-            print(b.board)
+            print(t) # Print the column for debugging
+            b.make_move(i) # Use the board object to make a move
+            game_state = b.win_exists() # Check if win exists within the board
+            board.print() # Print the board for debugging 
 
             return game_state, i #return which column
 
@@ -83,27 +78,8 @@ def send_Information( file, difficulty ):
     detections = detections[detections.class_id != 0] #filter out bad results
 
     pieces = [p for p in result['predictions'] if p['class'] in ['red','yellow','empty']] #sort pieces from result and current picture
-    sorted_pieces = sorted(pieces, key=lambda d: d['y'] + d['height'] / 2, reverse=False) #sort pieces by color
-    print(sorted_pieces)
-    grouped_data = [sorted_pieces[i:i + 7] for i in range(0, len(sorted_pieces), 7)] #group them then by class.
-
-    fully_sorted = [] #init sorted array
-    for group in grouped_data: #now put groups in sorted array
-        sorted_group = sorted(group, key=lambda d: d['x'], reverse=False) #sort individual group
-        fully_sorted.append(sorted_group) #add to array
-
-    board = "" #init board
-    for row in fully_sorted: #now go through sorted array and change into enums
-        for column in row: #go through each column and change into enums
-            piece = column['class'] #get current piece
-            if piece == 'red': #if is in red class
-                board += RED #add enum RED to board
-            elif piece == 'yellow': #if in yellow class
-                board += YELLOW #add enum YELLOW to board
-            else: # piece == 'empty':
-                board += '0' #add enum empty to board
-
-    print(board)
+    board = Board(pieces) # Construct board object from pieces
+    board.print() # DEBUG - print board
 
     move = determine_move( board, difficulty ) #call determine move function with my correct board and given difficulty
     return move #return what column to place game piece in.
--- src/board.py
+++ src/board.py
@@ -1,9 +1,12 @@
+from sklearn.cluster import KMeans # Used for detecting likely columns
+import numpy as np # Used for creating array for sklearn
+
 """
 Team 19
 Artifact: board.py
 Purpose: For using the board and performing operations on it
 Created: 3/17/25
-Revised: 3/17/25
+Revised: 4/16/25
 Preconditions: Ro-board is set up to have taken a picture from the board and a string has been made that represents the board
 Postconditions: Ro-board knows if a win has occurred
 Errors: None
@@ -11,6 +14,11 @@ Side Effects: None
 Invariants: None
 Faults: None
 """
+NEW_ROW_BOUND = 20 # Jumps to rows are indicated by a difference of 20 or more in the y-value
+NEXT_COL_BOUND = 40 # Jumps to columns are indicated by a difference of 100 or more in the x-value
+RED = '1'
+YELLOW = '2'
+
 class Board:
     
     """
@@ -33,8 +41,150 @@ class Board:
     """
 
     # Initialize board class
-    def __init__(self, board=None):
-        self.board = board
+    def __init__(self, pieces):
+        self.board = self.build(pieces) # Construct board with pieces
+
+    def build(self, pieces):
+        # Remove unneeded information
+        updated_pieces = [] # Keep track of new pieces
+        for piece in pieces:
+            # Find middle of the bounding box for more accurate representation
+            upd_piece = dict() # Empty piece
+            upd_piece['x'] = piece['x'] + (piece['width'] / 2) # Find x-coord of middle
+            upd_piece['y'] = piece['y'] + (piece['height'] / 2) # Find y-coord of middle
+            upd_piece['class'] = piece['class'] # Keep class
+            updated_pieces.append(upd_piece) # Add updated piece to update pieces
+
+        # Construct board from the pieces from the model with updated values
+        sorted_pieces = sorted(updated_pieces, key=lambda p: p['y'], reverse=False) #sort pieces by y value
+
+        grouped_data = [] # Stores all pieces sorted by row
+        prev_y = 0 # Keeps track of the previous y value
+        curr_row = [] # Keeps track of current row
+        for piece in sorted_pieces: # Loop through all the pieces
+            if prev_y == 0 or (abs(prev_y - piece['y']) < NEW_ROW_BOUND): # Add to the row if difference of y is small
+                curr_row.append(piece) # Add piece to current row
+            else:
+                grouped_data.append(curr_row) # Add row to grouped
+                curr_row = [] # Clear row
+                curr_row.append(piece)
+            prev_y = piece['y'] # Update previous y
+
+        grouped_data.append(curr_row) # Add last row to board
+
+
+        fully_sorted = [] #init sorted array
+        for group in grouped_data: #now put groups in sorted array
+            sorted_group = sorted(group, key=lambda d: d['x'], reverse=False) #sort individual group
+            fully_sorted.append(sorted_group) #add to array
+
+        # Remove duplicates from each row
+        no_duplicates = self._remove_duplicates(fully_sorted) 
+
+        board = "" #init board
+        for row in no_duplicates: #now go through sorted array and change into enums
+            for column in row: #go through each column and change into enums
+                piece = column['class'] #get current piece
+                if piece == 'red': #if is in red class
+                    board += RED #add enum RED to board
+                elif piece == 'yellow': #if in yellow class
+                    board += YELLOW #add enum YELLOW to board
+                else: # piece == 'empty':
+                    board += '0' #add enum empty to board
+
+        # Add pieces that are not detected
+        full_board = self._fill_pieces(no_duplicates, board)
+
+        return full_board
+
+    # Takes the board object and "fixes" it
+    # Fixing it means removing extra length (> 42 pieces in the board)
+    # and filling in gaps (< 42 pieces in the board)
+    def _fill_pieces(self, pieces, board):
+        xs = [] # Group for all x values
+        num_reds = 0 # Count the number of yellows in the board
+        num_yellows = 0 # Count the number of reds in the board
+        for row in pieces: # Loop through every row
+            for piece in row: # Loop through every piece
+                xs.append(piece['x']) # Add x to xs
+                if piece['class'] == 'red': # If piece of red
+                    num_reds += 1 # Add one to number of reds
+                elif piece['class'] == 'yellow': # If piece is yellow
+                    num_yellows += 1 # Add one to number of yellows 
+
+        xs_arr = np.array(xs).reshape(-1, 1) # Convert to 2D array
+        col_means = KMeans(n_clusters = 7, random_state=0).fit(xs_arr) # Find means of column x values
+        col_sorted = sorted(col_means.cluster_centers_.flatten())
+        # print("Column centers: ", col_sorted)
+
+        # Find missing pieces and add 0's in their place
+        missing_pieces = [] # Keeps track of the pieces that are missing (their index in board)
+        for i, row in enumerate(pieces):
+            if len(row) < 7: # Only look at rows well fewer than 7 pieces
+                assignments = [None] * 7  # 7 columns
+                for piece in row: # Loop through row of xs
+                    # Find closest column center
+                    x = piece['x']
+                    dists = [abs(x - c) for c in col_sorted] # Find distances to all columns
+                    col = dists.index(min(dists)) # Get column that this x value is closest to
+
+                    if assignments[col] is not None: # If piece already assigned to column
+                        unassigned = -1 # Try to find a column to the left that is unassigned
+                        for col_check in range(col, -1, -1): # Loop backwards through columns
+                            if assignments[col_check] is None: # Check if unassigned
+                                unassigned = col_check # Reassign unassigned
+                                break # End search
+                        if unassigned == -1: # Found no None
+                            for col_check in range(col, 8): # Search forward
+                                if assignments[col_check] is None: # Check if unassigned
+                                    assignments[col_check] = x # Assign col
+                                    break # End search
+                        else: # Found a None
+                            for col_swap in range(unassigned, col): # Shift assigned down 1
+                                assignments[col_swap] = assignments[col_swap + 1] # Shift col over
+
+                            assignments[col] = x # Update column of row with the value
+
+                    else: # Nothing assigned
+                        assignments[col] = x # Update column of row with the value
+
+                for j in range(len(assignments)):
+                    if assignments[j] == None:
+                        missing_pieces.append((7*i)+j)  
+        
+        missing_pieces = sorted(missing_pieces, reverse=True) # Reverse the missing pieces 
+        board_list = list(board)
+        for piece in missing_pieces: # Loop through missing pieces
+            if num_yellows <= num_reds: # Equal number of each color or less yellows 
+                if piece <= 34 and board_list[piece + 7] != '0': # If piece below is not empty
+                    board_list.insert(piece, YELLOW) # Yellow is missing and might go here
+                    num_yellows += 1 # Add 1 to num of yellow
+            elif num_yellows > num_reds + 1: # Too many yellows compared to reds
+                if piece <= 34 and board_list[piece + 7] != '0': # If piece below is not empty
+                    board_list.insert(piece, RED) # Red is missing and might go here
+                    num_reds += 1 # Add 1 to num of reds
+            else:
+                board_list.insert(piece, '0') # Piece is most likely empty
+    
+        board_list = "".join(board_list)
+        return board_list 
+
+    # Remove all duplicates from a row
+    def _remove_duplicates(self, pieces):
+        new_board = [] # Keep track of the new board
+        for row in pieces: # Loop through every row
+            new_row = [] # Keep track of the new row 
+            for i in range(len(row)): # Loop through row
+                if i < len(row) - 1 and abs(row[i + 1]['x'] - row[i]['x']) < NEXT_COL_BOUND: # Check next piece
+                    continue # Dont add to updated row 
+                new_row.append(row[i]) # Add piece to updated row
+            new_board.append(new_row)
+        return new_board # Return new board
+    
+    # Print the board to stdout
+    def print(self):
+        print(f"Board: {self.board}")
+        print(f"Length: {len(self.board)}")
 
     """ 
     Finds consecutive pieces in the column
@@ -178,7 +328,7 @@ class Board:
         # No 4 consecutive pieces on the board, so check for tie
         if '0' not in self.board:
             return 3    
-        return 0 # Just returrn no win
+        return 0 # Just return no win
 
     def make_move(self, column):
         # Loop backwards through column until an empty space is shown
@@ -186,3 +336,4 @@ class Board:
             if self[i*7 + column] == 0:
                 self[i*7 + column] = '2'
                 break
+    
