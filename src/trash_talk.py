9039efff9ccd3f657010a63d8cba814b8227a605
lecluyse2000 <lecluyse20@gmail.com>
2025-04-24 16:40:27 -0500

    Created a module for trash talk

---
+++ src/trash_talk.py
@@ -0,0 +1,30 @@
+import random
+
+class MyClass(GeneratedClass):
+    def __init__(self):
+        GeneratedClass.__init__(self)
+ 
+    def onLoad(self):
+        #put initialization code here
+        self.anim_speak = ALProxy("ALAnimatedSpeech")
+        self.tts = ALProxy("ALTextToSpeech")
+        pass
+ 
+    def onUnload(self):
+        #put clean-up code here
+        pass
+
+        # I cannot use cheographe on my mac so if these move the feet just change anim_speak to tts
+        # Or change to a different animation
+    def onInput_onStart(self):
+        random_integer = random.randint(0, 12) # Generate a random int
+        if random_integer >= 7 and random_integer < 10: # If the number is 7, 8, or 9 
+            self.anim_speak.say("^start(you) You are terrible! ^wait(you)") # Talk trash with an animation
+        elif  random_integer >= 10: # If the number is 10-12 
+            self.anim_speak.say("^start(not know) Are you sure about that move? ^wait(not know)") # Talk trash in a different way
+        else: # If the number is between 0 and 6
+            self.anim_speak.say("^start(happy) Nice move! ^wait(happy)") # Congratulate the user
+
+    def onInput_onStop(self):
+         self.onUnload() #it is recommended to reuse the clean-up as the box is stopped
+         self.onStopped() #activate the output of the box
