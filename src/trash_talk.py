5fecd9c4bc434b3de414a7785b841e1640d1a2c0
lecluyse2000 <lecluyse20@gmail.com>
2025-04-24 16:57:08 -0500

    More trash talk

--- src/trash_talk.py
+++ src/trash_talk.py
@@ -18,11 +18,15 @@ class MyClass(GeneratedClass):
         # Or change to a different animation
     def onInput_onStart(self):
         random_integer = random.randint(0, 12) # Generate a random int
-        if random_integer >= 7 and random_integer < 10: # If the number is 7, 8, or 9 
-            self.anim_speak.say("^start(you) You are terrible! ^wait(you)") # Talk trash with an animation
-        elif  random_integer >= 10: # If the number is 10-12 
+        if random_integer > 6 and random_integer < 10: # If the number is 7, 8, or 9 
+            self.anim_speak.say("^start(you) You are terrible at this game! ^wait(you)") # Talk trash with an animation
+        elif  random_integer == 10 or random_integer == 11: # If the number is 10 or 11
             self.anim_speak.say("^start(not know) Are you sure about that move? ^wait(not know)") # Talk trash in a different way
-        else: # If the number is between 0 and 6
+        elif  random_integer == 12: # If the number is 12 
+            self.anim_speak.say("^start(not know) Was that really the best move there? ^wait(not know)") # Talk trash in a different way
+        elif random_integer > 3 and random_integer < 7 : # If the number is 4, 5, or 6
+            self.anim_speak.say("^start(you) Good idea! ^wait(you)") # Congratulate the user
+        else: # If the number is 0-3
             self.anim_speak.say("^start(happy) Nice move! ^wait(happy)") # Congratulate the user
 
     def onInput_onStop(self):
